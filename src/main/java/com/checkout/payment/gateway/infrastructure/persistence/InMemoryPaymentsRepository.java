6d564ab3a714fc477149e53d3fb8d8213af06951
YSqr <tangyy.sh@gmail.com>
2025-12-16 22:42:37 +0800

    refine(repository): ensure atomicity with ReadWriteLock

--- src/main/java/com/checkout/payment/gateway/infrastructure/persistence/InMemoryPaymentsRepository.java
+++ src/main/java/com/checkout/payment/gateway/infrastructure/persistence/InMemoryPaymentsRepository.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Repository
 public class InMemoryPaymentsRepository implements PaymentsRepository {
@@ -14,26 +16,47 @@ public class InMemoryPaymentsRepository implements PaymentsRepository {
   private final Map<UUID, Payment> storage = new ConcurrentHashMap<>();
   private final Map<String, Payment> idempotencyIndex = new ConcurrentHashMap<>();
 
+  private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
   @Override
   public Payment save(Payment payment) {
-    if(payment.getId() == null) {
-      payment.setId(UUID.randomUUID());
-    }
-    storage.put(payment.getId(), payment);
+    lock.writeLock().lock();
+    try {
+      if(payment.getIdempotencyKey() != null && idempotencyIndex.containsKey(payment.getIdempotencyKey())) {
+        return idempotencyIndex.get(payment.getIdempotencyKey());
+      }
+      
+      if (payment.getId() == null) {
+        payment.setId(UUID.randomUUID());
+      }
+      storage.put(payment.getId(), payment);
 
-    if(payment.getIdempotencyKey() != null) {
-      idempotencyIndex.put(payment.getIdempotencyKey(), payment);
+      if (payment.getIdempotencyKey() != null) {
+        idempotencyIndex.put(payment.getIdempotencyKey(), payment);
+      }
+      return payment;
+    } finally {
+      lock.writeLock().unlock();
     }
-    return payment;
   }
 
   @Override
   public Optional<Payment> get(UUID id) {
-    return Optional.ofNullable(storage.get(id));
+    lock.readLock().lock();
+    try {
+      return Optional.ofNullable(storage.get(id));
+    } finally {
+      lock.readLock().unlock();
+    }
   }
 
   @Override
   public Optional<Payment> getByIdempotencyKey(String key) {
-    return Optional.ofNullable(idempotencyIndex.get(key));
+    lock.readLock().lock();
+    try {
+      return Optional.ofNullable(idempotencyIndex.get(key));
+    } finally {
+      lock.readLock().unlock();
+    }
   }
 }
